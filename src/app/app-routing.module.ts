414eb5a33a1a4cad7cf1ce553395941dae532612
juanysho <juanmonterocallealta@gmail.com>
2021-04-01 10:45:00 +0200

    Page not found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 // Modules
 import { PagesRoutingModule } from './pages/pages.routing';
 
-// import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-//   { path: '**', component: NopagefoundComponent },
+  { path: '**', component: PagenotfoundComponent },
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
